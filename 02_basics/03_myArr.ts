fa975e72612c2d0d82fc16c494279188cd61aae5
abhinikate <141385211+abhinikate@users.noreply.github.com>
2024-03-11 08:52:10 +0000

    Done

---
+++ 02_basics/03_myArr.ts
@@ -0,0 +1,21 @@
+const superHeros: string[] = []
+// const heroPower: number[] = []
+const heroPower: Array<number> = []
+
+type aUser = {
+    name: string
+    isActive: boolean
+}
+
+const allUsers: aUser[] = []
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
+
+
+superHeros.push("spiderman")
+heroPower.push(2)
+
+allUsers.push({name: "", isActive: true})
